17864d28418254bf3ea1ba526c4dd4acd6c2916d
DaffaPermadi <daffagt123@gmail.com>
2022-07-07 19:27:28 +0700

    aboutnya

---
+++ resources/views/about.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.main')
+@section('section')
+<div class="about">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <div class="about-img mt-3">
+                    <img src="image/about.jpg" alt="Image" style="width:40vw; border-radius:20px">
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="about-content">
+                    <div class="section-heade mt-5">
+                        <h2>New Innovation in The Order System</h2>
+                    </div>
+                    <div class="about-text mt-4">
+                        <p>
+                            In the era of increasingly advanced technological developments like today, people need a system that makes work easier. This includes the process of ordering food, and the existence of defects in transactions where the seller does not receive payment from the buyer and the length of the queue at the time of ordering. The reason for using the FIFO method in making this application is because in addition to many advantages such as the actual and relevant value of inventory costs, this system is expected to produce an application that helps in ordering, includes easy and transparent menu selection, and reduces queues when ordering.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec pretium mi. Curabitur facilisis ornare velit non vulputate. Aliquam metus tortor, auctor id gravida condimentum, viverra quis sem. Curabitur non nisl nec nisi scelerisque maximus. Aenean consectetur convallis porttitor. Aliquam interdum at lacus non blandit.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+  @endsection
